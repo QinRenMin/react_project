687e95b5663c1d11f42ebd0b50edb6a2a26973fa
QinRenMin <2622129594@qq.com>
2019-03-27 19:50:29 +0800

    update

--- src/view/index/data.js
+++ src/view/index/data.js
@@ -6,22 +6,28 @@ let data = {
         "title":"html基本框架",
         "last_reply_at":"2017-12-04T13:57:30.730Z",
         "img_url":"/bg1.jpg",
-        "author":"jane"
+        "author":"jane",
+        "reply_count":10,
+        "visit_count":1300
     },
         {
             "id":2,
             "title":"html行内与块级元素",
             "last_reply_at":"2017-12-05T13:57:30.730Z",
             "img_url":"/bg2.jpg",
-            "author":"lili"
+            "author":"lili",
+            "reply_count":20,
+            "visit_count":1000
         },
 
         {
             "id":3,
             "title":"htm全局属性",
-            "last_reply_at":"2017-12-17　13:57:30.73",
+            "last_reply_at":"2017-12-017T13:57:30.73",
             "img_url":"/fall.jpg",
-            "author":"Make"
+            "author":"Make",
+            "reply_count":50,
+            "visit_count":2000
         },
 
         {
@@ -29,7 +35,9 @@ let data = {
             "title":"html表格",
             "last_reply_at":"2017-12-019T13:57:30.730Z",
             "img_url":"/main5.jpg",
-            "author":"Lucy"
+            "author":"Lucy",
+            "reply_count":80,
+            "visit_count":100
         },
     ]
 };
--- src/view/index/list.js
+++ src/view/index/list.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import {List,Avatar} from "antd";
 import data from "./data";
 import {Link} from "react-router-dom";
+import TxtTag from "../txtTag"
 // import  arr from "../../images/bg1.jpg";
 class IndexList extends Component {
 
@@ -12,17 +13,26 @@ class IndexList extends Component {
             <List
                 dataSource = {data.data}
                 loading = {false}
-                renderItem = {(item) =>(<List.Item>
+                renderItem = {(item) =>(<List.Item
+
+                    actions = {[
+                        "回复："+item.reply_count,
+                        "访问："+item.visit_count
+                        ]}
+                >
+
 
                             <List.Item.Meta
                                  avatar={<Avatar src={require('../../images'+item.img_url)}/>}
                                  // avatar={<Avatar src={require("../../images/bg1.jpg")}/>}
                                  title ={
-                                     <h3>
-                                         <Link to="/details">
-                                        {item.title}
-                                         </Link>
-                                     </h3>
+
+                                        <div>
+                                            <TxtTag/>
+                                            <Link to="/details">
+                                                {item.title}
+                                            </Link>
+                                        </div>
                                  }
                                 description ={
                                      <p>
